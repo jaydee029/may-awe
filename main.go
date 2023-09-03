2a97cc5180d70decf3e14bd6e192e2a8ad3701c5
DEEJAY29 <dhruvj797@gmail.com>
2023-09-03 09:46:17 +0530

    api metrics added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -17,15 +18,37 @@ func corsmiddleware(app http.Handler) http.Handler {
 	})
 }
 
+func (cfg *apiconfig) reqcounts(app http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileservercounts++
+		app.ServeHTTP(w, r)
+	})
+
+}
+func (cfg *apiconfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileservercounts)))
+}
+
+type apiconfig struct {
+	fileservercounts int
+}
+
 func main() {
+	apicfg := apiconfig{
+		fileservercounts: 0,
+	}
+
 	port := "8100"
 	newmux := http.NewServeMux()
-	newmux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	newmux.Handle("/app/", apicfg.reqcounts(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	newmux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		w.Write([]byte(http.StatusText(http.StatusOK)))
 	})
+	newmux.HandleFunc("/metrics", apicfg.metrics)
 	sermux := corsmiddleware(newmux)
 
 	srv := &http.Server{
